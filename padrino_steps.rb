0ab7b07d6a94e6d21bd2d7f1d9b22d3bcee70162
Heath Padrick <hpadrick@comservecorp.com>
2011-04-22 14:35:52 -0400

    removed: padrino_steps

--- padrino_steps.rb
+++
@@ -1,6 +0,0 @@
-Given /^I log in as "([^"]*)"$/ do |user|
-  fill_in "Login", :with => user
-  fill_in "Password", :with => user + '-test-password'
-  click_button 'Sign In'
-  Then 'I should not see "Login"'
-end
